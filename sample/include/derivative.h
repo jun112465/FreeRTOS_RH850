64291c3a25c3a1ba15e9e5e60776d260076d0fdc
junyeob <junyeob@seonens.com>
2025-06-05 14:39:15 +0900

    1. DELETE : delete unnecessery files with CCRH 2. ADD : add comments to CMakeList files

--- sample/include/derivative.h
+++ sample/include/derivative.h
@@ -2,23 +2,11 @@
 #define DERIVATIVE_H
 
 #include "iodefine.h"
-
-#if defined(__GNUC__)
-#define __NOP() asm("nop")
-#define __DI() asm("di")
-#define __EI() asm("ei")
-#define __STSR(regID, selID) ({ long _val_; asm("stsr " #regID ", %[_val_], " #selID : [_val_]"=r"(_val_)); _val_; })
-#define __LDSR(regID, selID, val) asm("ldsr %[_val_]," #regID "," #selID ::[_val_] "r"(val))
-#elif defined(__ICCRH850__)
-#include <intrinsics.h>
-#elif defined(__CCRH__)
 #include <builtin.h>
+
 #define __NOP() __nop()
 #define __STSR(regID, selID) __stsr_rh(regID, selID)
 #define __LDSR(regID, selID, val) __ldsr_rh(regID, selID, val)
-#elif defined(__ghs__)
-#include <v800_ghs.h>
-#endif
 
 #define protected_write(preg, pstatus, reg, value) \
     do                                             \
