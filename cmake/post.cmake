b3d7c6f6d834e31f3ef5da28d043302cdd49a1e6
junyeob <junyeob@seonens.com>
2025-06-05 14:15:07 +0900

    DELETE : delete unnecessary files with CCRH build

--- cmake/post.cmake
+++ cmake/post.cmake
@@ -3,82 +3,20 @@
 
 set(CMAKE_EXECUTABLE_SUFFIX ".elf")
 
-if(${FREERTOS_TOOLCHAIN} STREQUAL "GCC")
+add_custom_command(TARGET
+    ${PROJECT_NAME}
+    POST_BUILD
+    ## Convert ELF to Motorola S-records
+    COMMAND
+    ${CMAKE_CCRH_LINKER}
+    ${PROJECT_NAME}.elf
+    -form=stype
+    -output=${PROJECT_NAME}.srec
+    ## Convert ELF to Raw Binary
+    COMMAND
+    ${CMAKE_CCRH_LINKER}
+    ${PROJECT_NAME}.elf
+    -form=binary
+    -output=${PROJECT_NAME}.bin
+)
 
-    add_custom_command(TARGET
-        ${PROJECT_NAME}
-        POST_BUILD
-        ## Convert ELF to Motorola S-records
-        COMMAND
-        ${CMAKE_OBJCOPY}
-        -O
-        srec
-        ${PROJECT_NAME}.elf
-        ${PROJECT_NAME}.srec
-        ## Convert ELF to Raw Binary
-        COMMAND
-        ${CMAKE_OBJCOPY}
-        -O
-        binary
-        ${PROJECT_NAME}.elf
-        ${PROJECT_NAME}.bin
-    )
-
-elseif(${FREERTOS_TOOLCHAIN} STREQUAL "IAR")
-
-    add_custom_command(TARGET
-        ${PROJECT_NAME}
-        POST_BUILD
-        ## Convert ELF to Motorola S-records
-        COMMAND
-        ${CMAKE_IAR_ELFTOOL}
-        --silent
-        ${PROJECT_NAME}.elf
-        --srec
-        ${PROJECT_NAME}.srec
-        ## Convert ELF to Raw Binary
-        COMMAND
-        ${CMAKE_IAR_ELFTOOL}
-        --silent
-        ${PROJECT_NAME}.elf
-        --bin
-        ${PROJECT_NAME}.bin
-    )
-
-elseif(${FREERTOS_TOOLCHAIN} STREQUAL "CCRH")
-
-    add_custom_command(TARGET
-        ${PROJECT_NAME}
-        POST_BUILD
-        ## Convert ELF to Motorola S-records
-        COMMAND
-        ${CMAKE_CCRH_LINKER}
-        ${PROJECT_NAME}.elf
-        -form=stype
-        -output=${PROJECT_NAME}.srec
-        ## Convert ELF to Raw Binary
-        COMMAND
-        ${CMAKE_CCRH_LINKER}
-        ${PROJECT_NAME}.elf
-        -form=binary
-        -output=${PROJECT_NAME}.bin
-    )
-
-elseif(${FREERTOS_TOOLCHAIN} STREQUAL "GHS")
-
-    add_custom_command(TARGET
-        ${PROJECT_NAME}
-        POST_BUILD
-        ## Convert ELF to Motorola S-records
-        COMMAND
-        ${CMAKE_GSREC}
-        ${PROJECT_NAME}.elf
-        -o
-        ${PROJECT_NAME}.srec
-        ## Convert ELF to Raw Binary
-        COMMAND
-        ${CMAKE_GMEMFILE}
-        ${PROJECT_NAME}.elf
-    )
-
-endif()
